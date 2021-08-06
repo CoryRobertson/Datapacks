cde76f4f0d28f1fdcf2ad0635e13478efed07fae
coryrobertson <cory.robertson17@gmail.com>
2021-08-05 20:28:46 -0700

    Further worked on learning more about objD

--- learningOBJD/index.dart
+++ learningOBJD/index.dart
@@ -1,10 +1,37 @@
 import 'package:objd/core.dart';
 
-main(List<String> args) {
+// main(List<String> args) {
+//   createProject(
+//       Project(
+//           name: "learningOBJD",
+//           target: "./",
+//           generate: Pack(name: "learningobjd")),
+//       args);
+// }
+void main(List<String> args) {
   createProject(
-      Project(
-          name: "learningOBJD",
-          target: "./",
-          generate: Pack(name: "learningobjd")),
-      args);
+    Project(
+      name: "This is going to be the generated folder name",
+      target: "./", // path for where to generate the project
+      generate: CustomWidget(), // The starting point of generation
+    ),
+    args,
+  );
+}
+
+class CustomWidget extends Widget {
+  @override
+  Widget generate(Context context) {
+    return Pack(
+        name: "mypack",
+        main: File(
+            // optional
+            'main',
+            //child: Log("Hello World!")
+            child: For.of([
+              Command('setblock 0 0 0 obsidian'),
+            ])
+            //
+            ));
+  }
 }
